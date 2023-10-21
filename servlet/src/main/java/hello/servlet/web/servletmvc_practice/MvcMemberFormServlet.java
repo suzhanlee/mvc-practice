093092b7c40407bfce92438760d3d9d721859c13
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-10-21 19:28:33 +0900

    feat : servlet-mvc-practice

---
+++ servlet/src/main/java/hello/servlet/web/servletmvc_practice/MvcMemberFormServlet.java
@@ -0,0 +1,24 @@
+package hello.servlet.web.servletmvc_practice;
+
+import hello.servlet.domain.member.MemberRepository;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
